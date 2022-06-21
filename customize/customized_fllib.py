d71b2467ed8bdf5d23718157dd3a3502747f84bb
yuxuanzh <yuxuanzh@h1.fl-dev.gaia-pg0.wisc.cloudlab.us>
2022-06-20 23:18:36 -0500

    add cifar10 dataset

--- customize/customized_fllib.py
+++ customize/customized_fllib.py
@@ -1,5 +1,7 @@
 from fedscale.core.arg_parser import args
 from fedscale.core.utils.utils_data import get_data_transform
+from torchvision import datasets
+import torch
 def init_dataset():
     # not support detection task or rl
     # only openimg dataset
@@ -10,5 +12,14 @@ def init_dataset():
         train_dataset = OpenImage(args.data_dir, dataset='train', transform=train_transform)
         test_dataset = OpenImage(args.data_dir, dataset='test', transform=test_transform)
         val_dataset = OpenImage(args.data_dir, dataset='validation', transform=test_transform)
+    if args.data_set == 'cifar10':
+        train_transform, test_transform = get_data_transform('cifar')
+        train_dataset = datasets.CIFAR10(args.data_dir, train=True, download=True,
+                                        transform=train_transform)
+        test_dataset = datasets.CIFAR10(args.data_dir, train=False, download=True,
+                                        transform=test_transform)
+        val_dataset = datasets.CIFAR10(args.data_dir, train=False, download=True,
+                                        transform=test_transform)
+
     return train_dataset, test_dataset, val_dataset
 
